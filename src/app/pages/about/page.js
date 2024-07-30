25bd717704f6738ecfddb6ee9c9b0a883f7e5846
dev4ajay <dev4ajaykumar@gmail.com>
2024-07-30 15:19:58 +0530

    first

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -1,8 +1,12 @@
 "use client"
 import Link from "next/link";
 import icondev from "../../../image/icon-dev.png";
-import iconapp from "../../../image/icon-app.png";
+// import iconapp from "../../../image/icon-app.png";
 import Image from "next/image";
+import { AiOutlineAntDesign } from "react-icons/ai";
+import { MdDeveloperMode } from "react-icons/md";
+import { FaAppStoreIos } from "react-icons/fa6";
+import { TbAppsFilled } from "react-icons/tb";
 
 export default function About() {
   return (
@@ -41,13 +45,14 @@ export default function About() {
             <div className="row flex flex-wrap gap-4">
               <div className="col-lg-6 service-item flex-1 min-w-[300px]">
                 <div className="flex gap-3 service-content-box p-4 border rounded-lg shadow-lg">
-                  <Image src={icondev} alt="design icon" width={40} height={20} />
+            
+                  <FaAppStoreIos className="fs"/>
                   <p className="service-item-text text-lg">App Development<br/>Professional development of applications.</p>
                 </div>
               </div>
               <div className="col-lg-6 service-item flex-1 min-w-[300px]">
                 <div className="flex gap-3 service-content-box p-4 border rounded-lg shadow-lg">
-                  <Image src={icondev} alt="design icon" width={40} height={20} />
+                  <MdDeveloperMode className="fs" />
                   <p className="service-item-text text-lg">Web Development<br/>High-quality development of sites at the professional level.</p>
                 </div>
               </div>
@@ -57,13 +62,13 @@ export default function About() {
             <div className="row flex flex-wrap gap-4">
               <div className="col-lg-6 service-item flex-1 min-w-[300px]">
                 <div className="flex gap-3 service-content-box p-4 border rounded-lg shadow-lg">
-                  <Image src={icondev} alt="design icon" width={40} height={20} />
+                <AiOutlineAntDesign className="fs" />
                   <p className="service-item-text text-lg">Web Design<br/>The most modern and high-quality design.</p>
                 </div>
               </div>
               <div className="col-lg-6 service-item flex-1 min-w-[300px]">
                 <div className="flex gap-3 service-content-box p-4 border rounded-lg shadow-lg">
-                  <Image src={icondev} alt="design icon" width={40} height={20} />
+                  <TbAppsFilled className="fs"/>
                   <p className="service-item-text text-lg">Photography<br/>I make high-quality photos in any category professionally.</p>
                 </div>
               </div>
--- src/app/pages/contact/page.js
+++ src/app/pages/contact/page.js
@@ -1,114 +1,44 @@
 "use client"
-import Nav from "../nav/page"
+
+import { BsFillSendFill } from "react-icons/bs";
+
 export default function Contact() {
     return (
       <div>
-    <header>
-  <h2 className="h2 article-title">Contact </h2>
-</header>
-<section className="bg-darks" id="contact">
-    <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div className="mb-4">
-            <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
-                <p className="text-base font-semibold uppercase tracking-wide text-white">
-                Contact Form
-
-                </p>
-                <h2
-                    className="font-heading mb-4 font-bold tracking-tight text-white text-3xl sm:text-5xl">
-                    Get in Touch
-                </h2>
-          
-            </div>
-        </div>
-        <div className="flex items-stretch justify-center">
-            <div className="grid md:grid-cols-2">
-                <div className="h-full pr-6">
-                    <p className="mt-3 mb-12 text-lg  text-white">
-                        Class aptent taciti sociosqu ad
-                        litora torquent per conubia nostra
-                    </p>
-                    <ul className="mb-6 md:mb-0">
-                        <li className="flex">
-                            <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="h-6 w-6">
-                                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
-                                    <path
-                                        d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z">
-                                    </path>
-                                </svg>
-                            </div>
-                            <div className="ml-4 mb-4">
-                                <h3 className="mb-2 text-lg font-medium leading-6 text-white">Our Address
-                                </h3>
-                                <p className=" text-white">Hisar ,  Haryana</p>
-                                <p className=" text-white">India</p>
-                            </div>
-                        </li>
-                        <li className="flex">
-                            <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="h-6 w-6">
-                                    <path
-                                        d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2">
-                                    </path>
-                                    <path d="M15 7a2 2 0 0 1 2 2"></path>
-                                    <path d="M15 3a6 6 0 0 1 6 6"></path>
-                                </svg>
-                            </div>
-                            <div className="ml-4 mb-4">
-                                <h3 className="mb-2 text-lg font-medium leading-6 text-white">Contact
-                                </h3>
-                                <p className=" text-white">Mobile: +917404701513 </p>
-                                <p className=" text-white">Mail: dev4ajaykumar@gmail.com</p>
-                            </div>
-                        </li>
-                        <li className="flex">
-                            <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" className="h-6 w-6">
-                                    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-                                    <path d="M12 7v5l3 3"></path>
-                                </svg>
-                            </div>
-                            <div className="ml-4 mb-4">
-                                <h3 className="mb-2 text-lg font-medium leading-6 text-white">Working
-                                    hours</h3>
-                                <p className=" text-white">Monday - Friday: 9:30 - 6:30</p>
-                              
-                            </div>
-                        </li>
-                    </ul>
+  <header>
+          <h2 className="h2 article-title">Contact</h2>
+        </header>
+<>
+<div className="h-screen ">
+    <div className="pt-10 md:pt-20">
+        <div className="p-4 md:p-8">
+            <h1 className="text-white  pb-8 font-light text-3xl">Contact form</h1>
+            <form className="flex flex-col items-center">
+                <div className="w-full">
+                    <div className="flex flex-col md:flex-row">
+                        <input id="name" type="text"
+                            className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full md:w-1/2 md:mr-2 outline-none focus:ring-2 focus:ring-red-300"
+                            placeholder="Name" required/>
+                        <input id="email" type="email"
+                            className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full md:w-1/2 md:ml-2 outline-none focus:ring-2 focus:ring-red-300"
+                            placeholder="Email" required/>
+                    </div>
+                    <input id="subject" type="text" placeholder="Subject"
+                        className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full outline-none focus:ring-2 focus:ring-red-300" required/>
+                    <textarea id="message" rows="5" placeholder="Say Something"
+                        className="my-2 py-2 px-4 rounded-md bg-gray-900 text-gray-300 w-full outline-none focus:ring-2 focus:ring-red-300" required/>
                 </div>
-                <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
-                    <h2 className="mb-4 text-2xl font-bold dark:text-white">Ready to Get Started?</h2>
-                    <form id="contactForm">
-                        <div className="mb-6">
-                            <div className="mx-0 mb-1 sm:mb-4">
-                                <div className="mx-0 mb-1 sm:mb-4">
-                                    <label for="name" className="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="name" autocomplete="given-name" placeholder="Your name" className="mb-2 w-full rounded-md border border-red-400 py-2 pl-2 pr-4 shadow-md bg-darks sm:mb-0" name="name"/>
-                                </div>
-                                <div className="mx-0 mb-1 sm:mb-4">
-                                    <label for="email" className="pb-1 text-xs uppercase tracking-wider"></label><input type="email" id="email" autocomplete="email" placeholder="Your email address" className="mb-2 w-full rounded-md border border-red-400 py-2 pl-2 pr-4 shadow-md bg-darks sm:mb-0" name="email"/>
-                                </div>
-                            </div>
-                            <div className="mx-0 mb-1 sm:mb-4">
-                                <label for="textarea" className="pb-1 text-xs uppercase tracking-wider"></label><textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." className="mb-2 w-full rounded-md border border-red-400 py-2 pl-2 pr-4 shadow-md bg-darks sm:mb-0"></textarea>
-                            </div>
-                        </div>
-                        <div className="text-center">
-                            <button type="submit" className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                <button
+                    className="border-2 text-md mt-5 rounded-md py-2 px-4 bg-blue-600 hover:bg-blue-700 text-gray-100 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-red-300">
+                  <div className="flex gap-2">
+                    <span><BsFillSendFill className="mt-1"/></span> <span>Send Message </span>
+                    </div>
+                </button>
+            </form>
         </div>
     </div>
-</section>
+</div>
+</>
       </div>
     );
   }
